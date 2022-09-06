1edfc1a7c390793255107f0ad94b997f468ba00b
largekind <3191524-largekind@users.noreply.gitlab.com>
2022-09-06 22:29:06 +0900

    画像識別用のサイトのベースアプリ作成

--- aiapps/aiapps/urls.py
+++ aiapps/aiapps/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import carbike.views as carbike
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('carbike/', carbike.index),
 ]
